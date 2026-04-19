9cb65ef44bd4ff5dc9045c867a4ba4780b9fc912
InvolutionMan <2096254817@qq.com>
2026-04-19 10:24:47 +0800

    feat(redis): 添加

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Query
 import redis.asyncio as redis
 from contextlib import asynccontextmanager
 from datetime import datetime
 from pydantic import BaseModel
+from enum import Enum
 
 # 全局 Redis 客户端
 redis_client = None
 
+class Direction(str, Enum):
+    left = "left"
+    right = "right"
 
 # 生命周期（替代 on_event）
 @asynccontextmanager
@@ -56,14 +60,18 @@ async def set_value(key: str, field: str,value: str, r=Depends(get_redis)):
 
 
 @app.get("/set_list")
-async def set_value(key: str, direction:str,value: str, r=Depends(get_redis)):
-    if direction == "left":
+async def set_value(
+    key: str,
+    direction: Direction = Query(..., description="选择插入方向：left=左侧插入，right=右侧插入"),
+    value: str = Query(..., description="要插入的值"),
+    r=Depends(get_redis)
+):
+    if direction == Direction.left:
         await r.lpush(key, value)
-        return {"msg": "插入成功"}
-    if direction == "right":
+        return {"msg": "从左边插入成功"}
+    else:
         await r.rpush(key, value)
-        return {"msg": "插入成功"}
-    return {"error": "direction must be left or right"}
+        return {"msg": "从右边插入成功"}
 
 @app.get("/set_set")
 async def set_value(key: str, value: str, r=Depends(get_redis)):
@@ -95,9 +103,39 @@ async def get_value(key: str, field: str = None, r=Depends(get_redis)):
         return {"type": "list", "value": value}
 
         # set
-        if key_type == "set":
-            value = await r.smembers(key)
-            return {"type": "set", "value": list(value)}
+    if key_type == "set":
+        value = await r.smembers(key)
+        return {"type": "set", "value": list(value)}
 
     return {"error": "Key not found or unsupported type"}
 
+@app.delete("/delete")
+async def delete_value(key: str, field: str = None, r=Depends(get_redis)):
+    key_type = await r.type(key)
+
+    # string
+    if key_type == "string":
+        await r.delete(key)
+        return {"msg": "删除成功"}
+
+    # hash
+    if key_type == "hash":
+        if field:
+            await r.hdel(key, field)
+            return {"msg": "删除成功"}
+        else:
+            await r.delete(key)
+            return {"msg": "删除成功"}
+
+    if key_type == "list":
+        await r.delete(key)
+        return {"msg": "删除成功"}
+
+    if key_type == "set":
+        if field:
+            await r.srem(key, field)
+            return {"msg": "删除成功"}
+        else:
+            await r.delete(key)
+            return {"msg": "删除成功"}
+    return {"error": "Key not found or unsupported type"}
